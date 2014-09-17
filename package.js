a1859db4f3a9f23d5621b8e61e44765fd058ea41
Brent Abrahams <brent_abrahams@yahoo.com>
2014-09-17 23:06:20 +0800

    Updated to 0.9 package system

--- package.js
+++ package.js
@@ -1,11 +1,16 @@
 Package.describe({
-  summary: "Undo/Redo stack based on transactions"
+  summary: "Undo/Redo stack based on transactions",
+  version: "0.2.0",
+  git: "https://github.com/jackadams/meteor-transactions.git",
+  name: "babrahams:transactions"
 });
 
 Package.on_use(function (api, where) {
 
+  api.versionsFrom("0.9.2.1");
+
   api.use('startup', 'client');
-  api.use('deps', 'client');
+  api.use('tracker', 'client');
   api.use('minimongo', 'client');
   api.use('templating', 'client');
   api.use('handlebars', 'client');

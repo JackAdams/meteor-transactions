dc079feb5a53ffdead75d2678b1d4c0f9948b214
Brent Abrahams <brent_abrahams@yahoo.com>
2014-09-19 15:42:47 +0800

    Updated to Meteor 0.9

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Undo/Redo stack based on transactions",
-  version: "0.2.0",
+  version: "0.2.1",
   git: "https://github.com/jackadams/meteor-transactions.git",
   name: "babrahams:transactions"
 });

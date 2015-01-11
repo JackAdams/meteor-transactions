282fd4e3d079e53c54ce19e8cd7b95bfff317488
Brent Abrahams <brent_abrahams@yahoo.com>
2015-01-12 07:10:30 +0800

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babrahams:transactions",
   summary: "Undo/Redo stack based on transactions",
-  version: "0.6.4",
+  version: "0.6.5",
   git: "https://github.com/jackadams/meteor-transactions.git"
 });
 

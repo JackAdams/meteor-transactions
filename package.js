b2664d4bf073b6d540ad024123dca4dbe1b44a93
Brent Abrahams <brent_abrahams@yahoo.com>
2015-01-11 16:43:00 +0800

    Fixed some issues

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
+  name: "babrahams:transactions",
   summary: "Undo/Redo stack based on transactions",
-  version: "0.6.0",
-  git: "https://github.com/jackadams/meteor-transactions.git",
-  name: "babrahams:transactions"
+  version: "0.6.1",
+  git: "https://github.com/jackadams/meteor-transactions.git"
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
 
   api.versionsFrom("1.0");
 
@@ -14,9 +14,10 @@ Package.on_use(function (api, where) {
   api.use('minimongo', 'client');
   api.use('templating', 'client');
   api.use('spacebars', 'client');
-  api.use('underscore', ['client','server']);
-  api.use('mongo', ['client','server']);
-  api.use('dburles:mongo-collection-instances@0.2.5', ['client','server']);
+  api.use('underscore');
+  api.use('mongo');
+  api.imply('mongo');
+  api.use('dburles:mongo-collection-instances@0.2.6');
 
   api.add_files('lib/transactions_client.html', 'client');
   api.add_files('lib/transactions_client.js', 'client');

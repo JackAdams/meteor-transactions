337c4bfa6f4cd7f6f2565e576ef61219ab27f4f2
Brent Abrahams <brent_abrahams@yahoo.com>
2015-01-22 22:48:36 +0800

    Made accounts-base a dependency

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babrahams:transactions",
   summary: "Undo/Redo stack based on transactions",
-  version: "0.6.7",
+  version: "0.6.8",
   git: "https://github.com/jackadams/meteor-transactions.git"
 });
 
@@ -16,6 +16,7 @@ Package.onUse(function (api, where) {
   api.use('spacebars', 'client');
   api.use('underscore');
   api.use('mongo');
+  api.use('accounts-base');
   api.imply('mongo');
   api.use('dburles:mongo-collection-instances@0.2.6');
 

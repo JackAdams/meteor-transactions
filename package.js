6f7ee8fdc6153fdb64771f0c155deb317739df8f
Brent Abrahams <brent_abrahams@yahoo.com>
2016-02-22 17:29:39 +0800

    Closes #48
    I hope it does, anyway.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "babrahams:transactions",
   summary: "App level transactions for Meteor + Mongo",
-  version: "0.7.6",
+  version: "0.7.7",
   git: "https://github.com/jackadams/meteor-transactions.git"
 });
 
